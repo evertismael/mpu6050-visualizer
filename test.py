0ca56c4239a526355c3f5ce983a7d2b8cc69a579
unknown <evertismael@gmail.com>
2021-04-14 21:37:15 +0200

    rere

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 # TODO:
 # Stream to a file, and plot the raw data in degrees and rad/sec
+
+import numpy as np
+roll = 1
+pitch = 2
+yaw = 3
+measurement = np.zeros((10,))
+measurement[1:4] = np.array([roll, pitch, yaw])
+print(measurement)
